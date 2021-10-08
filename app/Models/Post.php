d246345f74ba73ade46b33ed98701d8c91586435
Monika Kundu <mkundu993@gmail.com>
2021-10-09 02:56:57 +0600

    namespace added in models

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -5,6 +5,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+use App\Models\User;
+use App\Models\Category;
+use App\Models\Comment;
+use App\Models\Tag;
+
 class Post extends Model
 {
     use HasFactory;
